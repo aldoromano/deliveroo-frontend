36437a6bdc0f8dfcccdf5da17bed44e3051b9f97
aldoromano <tiano.roma@gmail.com>
2022-11-08 10:55:36 +0100

    Corrections

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import logo from "./assets/images/logo-teal.svg";
 
 function App() {
-  return (
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async () => {
+    const response = await axios.get("http://localhost:3200/");
+    // console.log(response.data);
+    setData(response.data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return !isLoading ? (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="header-container">
+          <img src={logo} alt="logo"></img>
+        </div>
       </header>
+      <main>
+        <div className="restaurant-container">
+          <div className="restaurant-left-container">
+            <h1>{data.restaurant.name}</h1>
+            <p>{data.restaurant.description}</p>
+          </div>
+          <div className="restaurant-right-container">
+            <img src={data.restaurant.picture}></img>
+          </div>
+        </div>
+
+        <div className="block-categories">
+          <div className="restaurant-left-container">
+            {data.categories.map((elem, index) => {
+              console.log("elem", elem);
+              return (
+                <div key={index} className="categories-container">
+                  <h2>{elem.name}</h2>
+
+                  <div className="categorie-container">
+                    {elem.meals.map((elem) => {
+                      return (
+                        <div key={elem.id} className="elem-container">
+                          {elem.title}
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              );
+            })}
+          </div>{" "}
+        </div>
+        <div className="restaurant-right-container"></div>
+      </main>
     </div>
+  ) : (
+    <span> En cours de chargement ... </span>
   );
 }
 
